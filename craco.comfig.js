34616ea94b0ffc48945fe253654651ca3858e09c
Rehnuma Reza (Deepty) <rehnuma.deepty@therapservices.net>
2025-08-19 14:36:56 +0600

    Changes in build configuration:
    * Remove Craco configurations and packages
    
    * Add vite configurations for deployment
    
    * Cleanup unnecessary packages

--- craco.comfig.js
+++
@@ -1,8 +0,0 @@
-export default {
-  webpack: {
-    configure: (webpackConfig) => {
-      webpackConfig.resolve.extensions = ['.js', '.jsx', '.ts', '.tsx', '.json'];
-      return webpackConfig;
-    },
-  },
-};
